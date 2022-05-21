d459f617a4de4736d5acb6e7f57427e77b753337
Humberto Albarrán <hjalbarran@gmail.com>
2022-05-21 19:07:30 +0200

    .

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,28 @@
+import axios from '@/services/axios'
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    loadingStatus: 'notLoading',
+    todoItems: []
   },
   getters: {
   },
   mutations: {
+    SET_LOADING_STATUS(state, status) {
+      state.loadingStatus = status
+    },
+    SET_TODOS(state, todoItems) {
+      state.todoItems = todoItems
+    }
   },
   actions: {
+    fetchToDoItems(context) {
+      axios.get('/todoItems')
+        .then((response) => {
+          commit('SET_TODOS', response.data)
+        })
+    }
   },
   modules: {
   }
