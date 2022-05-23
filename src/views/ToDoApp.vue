4bebf88c4e277a899c3519d4df7cb96e733df3c0
Humberto Albarrán <hjalbarran@gmail.com>
2022-05-23 14:45:21 +0200

    Splitted CSS, and clean db.json

--- src/views/ToDoApp.vue
+++ src/views/ToDoApp.vue
@@ -9,7 +9,7 @@
       <!-- FOOTER hidden: when no todos. Receives count of completed and uncompleted tasks, and also the dynamic filtering/routing-->
       <ToDoListFooter :unCompletedTasks="todoItemUnCompleted.length" :completedTask="todoItemCompleted.length" v-if="todoItems.length > 0" v-on:filterByCompleted="filterByCompletedMethod"/>
     </section> 
-    <footer >
+    <footer class="info">
 			<p>Done by <a href="https://github.com/hjalbarran" target="_blank">Humberto Albarrán</a> @Madrid, Spain</p>
 		</footer>
 
@@ -70,3 +70,27 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.info {
+	margin: 65px auto 0;
+	color: #bfbfbf;
+	font-size: 10px;
+	text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
+	text-align: center;
+}
+
+.info p {
+	line-height: 1;
+}
+
+.info a {
+	color: inherit;
+	text-decoration: none;
+	font-weight: 400;
+}
+
+.info a:hover {
+	text-decoration: underline;
+}
+</style>
